c3328c07568dbfbbb172d9a6d3c9e3b57b8ec764
neurotronix <code@mauermann.io>
2020-06-08 20:34:34 +0200

    feat(macos): add dock and key repeat tweaks

--- macos/tweaks.sh
+++ macos/tweaks.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
 # remove dock display delay
-defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
+defaults write com.apple.dock autohide-time-modifier -float 0.4 && \
+defaults write com.apple.dock autohide-delay -float 0 && \
+killall Dock
+
+# key repeat for vs code vim
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# key repeat speed
+defaults write -g KeyRepeat -int 1
