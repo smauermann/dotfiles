8243e9ba9294d18113460034719ba09c39d526c3
smauermann <github@mauermann.io>
2022-08-26 22:48:51 +0200

    feat: bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Setting up new machine"
+read -p "Press enter to continue"
+
+./brew/setup.sh
+./zsh/setup.sh
+./git/setup.sh
+./vim/setup.sh
+./kitty/setup.sh
+./macos/setup.sh
