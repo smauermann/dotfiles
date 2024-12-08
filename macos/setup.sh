57079d748701ce42d356d79b3b2557132809ea4b
smauermann <github@mauermann.io>
2024-12-08 20:25:43 +0100

    feat(macos): autohide dock

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # remove dock display delay
+defaults write com.apple.dock autohide -bool true && \
 defaults write com.apple.dock autohide-time-modifier -float 0.4 && \
 defaults write com.apple.dock autohide-delay -float 0 && \
 killall Dock
@@ -9,5 +10,5 @@ killall Dock
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
 # key repeat speed
-defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 0.5 # normal minimum is 2 (30 ms)
