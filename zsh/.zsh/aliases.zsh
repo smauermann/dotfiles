c0c958e4d82da5650d4e07042fdc6080da516f39
smauermann <github@mauermann.io>
2026-03-14 14:38:42 +0100

    chore: clean up personal aliases
    Remove unused aliases (zshconfig, zshsource, vimconfig) and ky
    function. Group by tool.

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,18 +1,9 @@
-alias zshconfig="vim ~/.zshrc"
-alias zshsource="source ~/.zshrc"
-alias vimconfig="vim ~/.vimrc"
-alias kevent="kubectl get events --sort-by='.lastTimestamp' -w"
 alias tg="terragrunt"
 alias tf="tofu"
+
 alias kn=kubens
+alias kevent="kubectl get events --sort-by='.lastTimestamp' -w"
 alias kx=kubectx
+
 alias c=claude
 alias cx="claude --dangerously-skip-permissions"
-
-function ky() {
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "Usage: ky <resource_type> <resource_name>"
-        return 1
-    fi
-    kubectl get "$1" "$2" -o yaml | yq
-}
