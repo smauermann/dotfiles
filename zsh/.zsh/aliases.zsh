41b9cf89e99949ad14cee7d897fffaea3ec74230
smauermann <github@mauermann.io>
2026-03-14 15:01:20 +0100

    feat: add grt and grep aliases, sort alphabetically

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,9 +1,13 @@
-alias tg="terragrunt"
-alias tf="tofu"
-
-alias kn=kubens
-alias kevent="kubectl get events --sort-by='.lastTimestamp' -w"
-alias kx=kubectx
-
 alias c=claude
 alias cx="claude --dangerously-skip-permissions"
+
+alias grep=rg
+
+alias grt='cd $(git rev-parse --show-toplevel)'
+
+alias kevent="kubectl get events --sort-by='.lastTimestamp' -w"
+alias kn=kubens
+alias kx=kubectx
+
+alias tf="tofu"
+alias tg="terragrunt"
