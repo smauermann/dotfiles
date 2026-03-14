9ffc4cc7f1fefcdf67ed8d825da63a64485a7860
smauermann <github@mauermann.io>
2026-03-14 15:28:47 +0100

    feat: add gcl, gcm, agrep aliases, remove redundant grep=rg

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,8 +1,16 @@
 alias c=claude
 alias cx="claude --dangerously-skip-permissions"
 
-alias grep=rg
+alias agrep='alias | rg'
 
+gcl() {
+  local url="$1"
+  local repo_path
+  repo_path=$(echo "$url" | sed -E 's#^(https?://|git@)##; s#:#/#; s#\.git$##')
+  local dest="$HOME/projects/$repo_path"
+  git clone "$url" "$dest" && cd "$dest"
+}
+alias gcm='git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@")'
 alias grt='cd $(git rev-parse --show-toplevel)'
 
 alias kevent="kubectl get events --sort-by='.lastTimestamp' -w"
