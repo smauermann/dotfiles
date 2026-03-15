ab4709ebe4ad2b580897e249e37b76bce3723ffd
smauermann <github@mauermann.io>
2026-03-15 10:49:55 +0100

    feat: switch from zimfw git to omz git plugin, add gclp alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -3,15 +3,18 @@ alias cx="claude --dangerously-skip-permissions"
 
 alias agrep='alias | rg'
 
-gcl() {
+# omz git plugin dependency (normally in omz lib/git.zsh)
+function git_current_branch() {
+  git rev-parse --abbrev-ref HEAD 2>/dev/null
+}
+
+gclp() {
   local url="$1"
   local repo_path
   repo_path=$(echo "$url" | sed -E 's#^(https?://|git@)##; s#:#/#; s#\.git$##')
   local dest="$HOME/projects/$repo_path"
   git clone "$url" "$dest" && cd "$dest"
 }
-alias gcm='git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@")'
-alias grt='cd $(git rev-parse --show-toplevel)'
 
 alias kevent="kubectl get events --sort-by='.lastTimestamp' -w"
 alias kn=kubens
