8d2cb68491f479811adcd5462f3bffd37e0e2645
smauermann <github@mauermann.io>
2026-03-17 22:26:46 +0100

    fix: cd to script dir so setup.sh works from any cwd

--- mise/setup.sh
+++ mise/setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
 source ../utils.sh
 mkdir -p ~/.config/mise
 safe_link $PWD/config.toml ~/.config/mise/config.toml
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
 source ../utils.sh
 
 CONFIG_DIR=$HOME/.config
